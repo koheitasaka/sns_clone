dc21db10c8c61b407b67b4929504ebdd7ff289a5
Kohei Tasaka <soundasleep@keio.jp>
2019-03-12 18:55:15 +0900

    timeline done

---
+++ snsclone/app/controllers/relationships_controller.rb
@@ -0,0 +1,21 @@
+class RelationshipsController < ApplicationController
+	before_action :find_user
+	
+	def create
+		unless current_user == @user
+			@relationship = current_user.relationships.create(follow_id: @user.id)
+			redirect_to user_path(@user)
+		end
+	end
+
+	def destroy
+		@relationship = Relationship.find_by(user_id: current_user.id, follow_id: @user.id)
+		@relationship.destroy
+		redirect_to user_path(@user)
+	end
+
+	private 
+	def find_user
+		@user = User.find(params[:user_id])
+	end
+end
