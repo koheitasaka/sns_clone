ececae4c0b20f174402f53e0ae65116ce8b556a5
Kohei Tasaka <soundasleep@keio.jp>
2019-03-19 19:40:53 +0900

    account suspension done

--- snsclone/app/controllers/application_controller.rb
+++ snsclone/app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource)
+    tweets_path 
+  end
   protected
 
   def configure_permitted_parameters
