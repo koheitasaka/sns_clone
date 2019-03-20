15bb9d477f8a68dd5063393dea785d845e93173a
Kohei Tasaka <soundasleep@keio.jp>
2019-03-21 07:06:53 +0900

    popup done

--- snsclone/config/routes.rb
+++ snsclone/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  
+  devise_for :users, controllers: { sessions: 'users/sessions'}
+
   resources :tweets do
     resources :likes, only: [:create, :destroy]
   end
